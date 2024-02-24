174e3e2386524e22e53255ab5c4b338cebed1a0c
VoCongKhoa <vocongkhoa2208@gmail.com>
2024-02-25 01:02:08 +0700

    Fix get FC as HTML

--- src/main/java/kutilities/domain/constants/wfh/WFHConstants.java
+++ src/main/java/kutilities/domain/constants/wfh/WFHConstants.java
@@ -2,6 +2,22 @@ package kutilities.domain.constants.wfh;
 
 public class WFHConstants {
     public static final String COMMA = ",";
-    public static final String TAG_OPEN_BOLD = "<b>";
-    public static final String TAG_CLOSE_BOLD = "</b>";
+    public static final String TAG_BOLD_OPEN = "<b>";
+    public static final String TAG_BOLD_CLOSE = "</b>";
+    public static final int NO = 0;
+    public static final int ID = 0;
+    public static final int FRONT_VALUE = 1;
+    public static final int BACK_VALUE = 2;
+    public static final int MODULE = 3;
+    public static final int TARGET_SKILL_NAME = 4;
+    public static final String DOUBLE_QUOTE_OPEN_CLOSE = "\"\"";
+    public static final String VERTICAL_BAR = "\\|";
+
+    public static final String TAG_NAME_CARD = "card";
+    public static final String TAG_NAME_TERMS = "terms";
+    public static final String TAG_NAME_DEF = "def";
+    public static final String TAG_NAME_SET = "set";
+    public static final String ATTRIBUTE_NAME_ID = "id";
+    public static final String ATTRIBUTE_NAME_CATEGORY = "category";
+    public static final String ATTRIBUTE_NAME_AUDIO = "audio";
 }
